cc5926e3e4061a10885c307a56f3aff7e172e920
TABET DJALAL <dtabet@oppido.fr>
2020-03-04 21:24:53 +0100

    ngIf

--- src/app/main-dash/main-dash.component.ts
+++ src/app/main-dash/main-dash.component.ts
@@ -1,33 +1,44 @@
-import { Component } from '@angular/core';
-import { map } from 'rxjs/operators';
-import { Breakpoints, BreakpointObserver } from '@angular/cdk/layout';
+import {Component, OnInit} from '@angular/core';
+import {map} from 'rxjs/operators';
+import {Breakpoints, BreakpointObserver} from '@angular/cdk/layout';
+import {QuickLunchService} from '../services/quick-lunch.service';
+import {Food} from '../models/food.interface';
 
 @Component({
   selector: 'app-main-dash',
   templateUrl: './main-dash.component.html',
   styleUrls: ['./main-dash.component.css']
 })
-export class MainDashComponent {
-  /** Based on the screen size, switch from standard to one column per row */
+export class MainDashComponent implements OnInit {
+
+  burgers: Food[];
+  pizzas: Food[];
+  galettes: Food[];
+
   cards = this.breakpointObserver.observe(Breakpoints.Handset).pipe(
-    map(({ matches }) => {
+    map(({matches}) => {
       if (matches) {
         return [
-          { title: 'A la carte', cols: 1, rows: 1 },
-          { title: 'Burgers', cols: 1, rows: 1 },
-          { title: 'Galettes/crêpes', cols: 1, rows: 1 },
-          { title: 'Pizzas', cols: 1, rows: 1 }
+          { title: "Burgers", cols: 2, rows: 1, id: 'brg' },
+          { title: "Galettes/crêpes", cols: 2, rows: 1, id: 'glt' },
+          { title: "Pizza", cols: 2, rows: 1, id: 'pzz' }
         ];
       }
 
       return [
-        { title: 'A la carte', cols: 2, rows: 1 },
-        { title: 'Burgers', cols: 1, rows: 1 },
-        { title: 'Galettes/crêpes', cols: 1, rows: 2 },
-        { title: 'Pizzas', cols: 1, rows: 1 }
+        { title: "Burgers", cols: 2, rows: 1, id: 'brg' },
+        { title: "Galettes/crêpes", cols: 2, rows: 1, id: 'glt' },
+        { title: "Pizza", cols: 2, rows: 1, id: 'pzz' }
       ];
     })
   );
 
-  constructor(private breakpointObserver: BreakpointObserver) {}
+  constructor(private breakpointObserver?: BreakpointObserver, private qls?: QuickLunchService) {
+  }
+
+  ngOnInit(): void {
+    this.burgers = this.qls.burgers;
+    this.pizzas = this.qls.pizzas;
+    this.galettes = this.qls.galettes;
+  }
 }
