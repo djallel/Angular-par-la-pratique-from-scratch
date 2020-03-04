154a0fe6a1c232aebaf5c46ae8faf9faa0c731b3
TABET DJALAL <dtabet@oppido.fr>
2020-03-04 16:46:29 +0100

    front material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,68 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes, Router } from "@angular/router";
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MainNavComponent } from './main-nav/main-nav.component';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import {MatCardModule} from '@angular/material/card';
+import { HomeComponent } from './home/home.component';
+import { CarteComponent } from './carte/carte.component';
+import { ReserveComponent } from './reserve/reserve.component';
+import { ContactComponent } from './contact/contact.component';
+import { MainDashComponent } from './main-dash/main-dash.component';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatMenuModule } from '@angular/material/menu';
+import { DishTableComponent } from './dish-table/dish-table.component';
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSortModule } from '@angular/material/sort';
+
+
+const routes: Routes = [
+  { path: "", component: HomeComponent },
+  { path: "carte", component: CarteComponent },
+  { path: "reserve", component: ReserveComponent },
+  //{ path: "schedule", component: ScheduleComponent },
+  { path: "contact", component: ContactComponent }
+];
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainNavComponent,
+    HomeComponent,
+    CarteComponent,
+    ReserveComponent,
+    ContactComponent,
+    MainDashComponent,
+    DishTableComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    LayoutModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule,
+    MatCardModule,
+    RouterModule.forRoot(routes),
+    MatGridListModule,
+    MatMenuModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/main-dash/main-dash.component.ts
@@ -0,0 +1,33 @@
+import { Component } from '@angular/core';
+import { map } from 'rxjs/operators';
+import { Breakpoints, BreakpointObserver } from '@angular/cdk/layout';
+
+@Component({
+  selector: 'app-main-dash',
+  templateUrl: './main-dash.component.html',
+  styleUrls: ['./main-dash.component.css']
+})
+export class MainDashComponent {
+  /** Based on the screen size, switch from standard to one column per row */
+  cards = this.breakpointObserver.observe(Breakpoints.Handset).pipe(
+    map(({ matches }) => {
+      if (matches) {
+        return [
+          { title: 'A la carte', cols: 1, rows: 1 },
+          { title: 'Burgers', cols: 1, rows: 1 },
+          { title: 'Galettes/crêpes', cols: 1, rows: 1 },
+          { title: 'Pizzas', cols: 1, rows: 1 }
+        ];
+      }
+
+      return [
+        { title: 'A la carte', cols: 2, rows: 1 },
+        { title: 'Burgers', cols: 1, rows: 1 },
+        { title: 'Galettes/crêpes', cols: 1, rows: 2 },
+        { title: 'Pizzas', cols: 1, rows: 1 }
+      ];
+    })
+  );
+
+  constructor(private breakpointObserver: BreakpointObserver) {}
+}
